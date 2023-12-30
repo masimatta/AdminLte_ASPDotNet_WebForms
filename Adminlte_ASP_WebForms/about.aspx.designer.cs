f863f2af1e6018d40fe9006fca2c944456c30201
masimatta <masimatta@gmail.com>
2023-12-30 08:57:11 +0500

    updated

--- Adminlte_ASP_WebForms/about.aspx.designer.cs
+++ Adminlte_ASP_WebForms/about.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace Adminlte_ASP_WebForms
 
     public partial class about
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
---
+++ Adminlte_ASP_WebForms/side.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Adminlte_ASP_WebForms
+{
+    public partial class side : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
